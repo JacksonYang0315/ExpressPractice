84599d12b6d7a0069648731b042a76839a50f091
yaayaya <yso7234189@gmail.com>
2019-03-27 21:46:16 +0800

    add CRUD

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,18 +1,72 @@
-const {User} = require('../models')
+const {
+    User
+} = require('../models')
 
-module.exports ={
-    async create (req, res){
-        console.log(req.body)
-        try{
+module.exports = {
+    async create(req, res) {
+        try {
             const user = await User.create(req.body)
             res.send(user)
-        }catch(err){
+        } catch (err) {
             res.status(500).send({
                 error: '創造user時有錯誤發生'
             })
         }
+    },
+    async index(req, res) {
+        try {
+            const users = await User.findAll()
+            res.send(users)
+        } catch (err) {
+            res.status(500).send({
+                error: '查詢users時有錯誤發生'
+            })
+        }
+    },
+    async show(req, res) {
+        try {
+            const users = await User.findByPk(req.params.userId)
+            res.send(users)
+        } catch (err) {
+            res.status(500).send({
+                error: '查詢user時有錯誤發生'
+            })
+        }
+    },
+    async update(req, res) {
+        try {
+            await User.update(req.body, {
+                where: {
+                    id: req.params.userId
+                }
+            })
+            res.send(req.body)
+        } catch (err) {
+            res.status(500).send({
+                error: '更新user時有錯誤發生'
+            })
+        }
+    },
+    async delete(req, res) {
+        try {
+            const user = await User.find({
+                where: {
+                    id: req.params.userId
+                }
+            })
+            await user.update({
+                isDelete:true
+            })
 
+
+
+
+        } catch (err) {
+            res.status(500).send({
+                error: '刪除user時有錯誤發生'
+            })
+        }
     }
-    
+
 
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,23 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController')
 
-// router.post('/POST/user', UserController.create);
-
 router.route('/POST/user')
-.post(UserController.create)
+    .post(UserController.create)
+// .get(UserController.create)
+
+// router.post('/POST/user',UserController.create)
+// router.get('/POST/user', UserController.create)
+
+router.route('/GET/users')
+    .get(UserController.index)
+
+router.route('/GET/users/:userId')
+    .get(UserController.show)
+
+router.route('/PUT/users/:userId')
+    .put(UserController.update)
+
+router.route('/DELETE/users/:userId')
+    .delete(UserController.delete)
 
 module.exports = router;
